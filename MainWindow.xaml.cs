393f705fe159c5fd5ca741d82e41a7990b465865
Dean <dean.marinov@gmx.at>
2021-10-27 22:06:33 +0200

    keine Ahnung

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,10 +11,22 @@ namespace primetest_gmp
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		string[] terms = new string[100];
+		string[] memory;
+		string[] term_split;
+		string[] resultstring = new string[2];
+		float[] term_result;
+		char[] separator = { '+', '-', '*', '/' };
+		int result_index = 0;
+		int memory_index = 0;
+		int split_index = 0;
 		public MainWindow()
 		{
 			InitializeComponent();
+			string input = OutputScreen_green.Text.ToString();
+			memory = new string[input.Length];
+			term_result = new float[4];
+			term_split = new string[input.Length];
+			TermOutput(TermEvaluation(OutputScreen_green.Text.ToString()));
 		}
 		private void btn_write_click(object sender, RoutedEventArgs e)
 		{
@@ -58,104 +70,198 @@ namespace primetest_gmp
 			}
 		}
 
-		private string BracketsResolve(string input)
+		private string BracketsResolve(string input) //returns the term in the most inner brackets
 		{
-			string[] resultstring = new string[100];
-			string temp;
-			int n = 0;
-			string inline = input;
-			char[] literals = inline.ToCharArray();
-			for(int i = 0; i < literals.Length - 1; i++)
+			if(input.Contains('('))
 			{
-				if(literals[i] == '(')
-				{
-					resultstring = inline.Split('(', 2);
-					terms[n] = resultstring[n];
-					n++;
-				}
+				resultstring = input.Split('(', 2);
+				term_split[split_index++] = resultstring[0];
+				input = resultstring[1];
+				return BracketsResolve(input);
 			}
-			temp = resultstring[n];
-			literals = temp.ToCharArray();
-			for(int j = 0; j < literals.Length-1; j++)
+			else if(input.Contains(')'))
 			{
-				n = 0;
-				if(temp.EndsWith(')'))
+				resultstring = input.Split(')', 2);
+				term_split[split_index++] = resultstring[1];
+				input = resultstring[0];
+				return BracketsResolve(input);
+			}
+			term_split[0] = string.Concat(term_split[0], term_split[1]);
+			term_split[1] = null;
+			return TermResolve(input);
+		}
+		private string TermAdd(string input)
+		{
+			if(input.Contains('+'))
+			{
+				resultstring = input.Split('+', 2);
+				memory[memory_index++] = resultstring[0];
+				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
-					temp = temp[0..^1];
-					resultstring[n] = temp;
+					term_result[result_index] = int.Parse(resultstring[0]) + int.Parse(resultstring[1]);
+					return term_result[result_index++].ToString();
+				}
+				return TermAdd(resultstring[1]);
+			}
+			for(int i = 0; i < memory.Length - 1; i++)
+			{
+				if(memory[i] != null)
+				{
+					term_result[result_index] += int.Parse(memory[i]);
+					memory[i] = null;
+				}
+				else
+				{
+					result_index++;
 					break;
 				}
-				if(literals[j] == ')')
+			}
+			return TermResolve(resultstring[1]);
+		}
+		private string TermSub(string input)
+		{
+			if(input.Contains('-'))
+			{
+				resultstring = input.Split('-', 2);
+				memory[memory_index++] = resultstring[0];
+				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
-					resultstring = temp.Split(')', 2); //rs[0] = middleterm
-					n++;
-					terms[n] = resultstring[n]; 
+					term_result[result_index] = int.Parse(resultstring[0]) - int.Parse(resultstring[1]);
+					return TermResolve(term_result[result_index++].ToString());
+				}
+				return TermSub(resultstring[1]);
+			}
+			for(int i = 0; i < memory.Length - 1; i++)
+			{
+				if(memory[i] != null)
+				{
+					term_result[result_index] -= int.Parse(memory[i]);
+					memory[i] = null;
+				}
+				else
+				{
+					result_index++;
+					break;
 				}
 			}
-			return resultstring[n];
+			return TermResolve(resultstring[1]);
+		}
+		private string TermMul(string input)
+		{
+			if(input.Contains('*'))
+			{
+				resultstring = input.Split('*', 2);
+				memory[memory_index++] = resultstring[0];
+				while(!resultstring[0].All(char.IsDigit))
+				{
+					foreach(char element in separator)
+					{
+						if(resultstring[0].Contains(element))
+						{
+							memory = resultstring[0].Split(element, 2);
+							resultstring[0] = memory[1];
+							break;
+						}
+					}
+				}
+				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
+				{
+					term_result[result_index] = int.Parse(resultstring[0]) * int.Parse(resultstring[1]);
+					return TermResolve(term_result[result_index++].ToString());
+				}
+				return TermMul(resultstring[1]);
+			}
+			for(int i = 0; i < memory.Length - 1; i++)
+			{
+				if(memory[i] != null)
+				{
+					term_result[result_index] *= int.Parse(memory[i]);
+					memory[i] = null;
+				}
+				else
+				{
+					result_index++;
+					break;
+				}
+			}
+			return TermResolve(resultstring[1]);
+		}
+		private string TermDiv(string input)
+		{
+			if(input.Contains('/') && resultstring[0].All(char.IsDigit))
+			{
+				resultstring = input.Split('/', 2);
+				memory[memory_index++] = resultstring[0];
+				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
+				{
+					term_result[result_index] = int.Parse(resultstring[0]) / int.Parse(resultstring[1]);
+					return term_result[result_index++].ToString();
+				}
+				return TermDiv(resultstring[1]);
+			}
+			for(int i = 0; i < memory.Length - 1; i++)
+			{
+				if(memory[i] != null)
+				{
+					term_result[result_index] /= int.Parse(memory[i]);
+					memory[i] = null;
+				}
+				else
+				{
+					result_index++;
+					break;
+				}
+			}
+			return TermResolve(resultstring[1]);
+		}
+		private string TermResolve(string input) //returns the result of a blank term
+		{
+			if(input.Contains('*'))
+			{
+				TermMul(input);
+			}
+			memory_index = 0;
+			if(input.Contains('/'))
+			{
+				TermDiv(input);
+			}
+			memory_index = 0;
+			if(input.Contains('+'))
+			{
+				TermAdd(input);
+			}
+			memory_index = 0;
+			if(input.Contains('-'))
+			{
+				TermSub(input);
+			}
+			memory_index = 0;
+			for(int i = 1; i < term_result.Length - 1; i++)
+			{
+				term_result[0] += term_result[i];
+				term_result[i] = 0;
+			}
+			input = string.Concat(term_split[0], term_result[0]);
+			term_split[0] = null;
+			term_split[1] = null;
+			return TermResolve(input);
 		}
 
-		private string Terms(string input)
+		private void TermOutput(string term)
 		{
-			string[] adds = new string[100];
-			string[] subs = new string[100];
-			int result = 0, k = 0, l = 0;
-			string term = input;
-			string[] resultstring = new string[100];
-			char[] literals = term.ToCharArray();
-			if(term.Contains('*'))
-			{
-				for(int i = 0; i < literals.Length - 1; i++)
-				{
-					if(literals[i] == '*')
-					{
-						resultstring = term.Split('*');
-						result = int.Parse(resultstring[0]) * int.Parse(resultstring[1]);
-					}
-					if(literals[i] == '/')
-					{
-						resultstring = term.Split('/');
-						result = int.Parse(resultstring[0]) / int.Parse(resultstring[1]);
-					}
-				}
-				return result.ToString(); //term x*y or x/y
-			}
-			else
-			{
-				for(int j = 0; j < literals.Length - 1; j++)
-				{
-					if(literals[j] == '+')
-					{
-						resultstring = term.Split('+', 2);
-						adds[k] = resultstring[k];
-						term = resultstring[k+1];
-						k++;
-					}
-					if(literals[j] == '-')
-					{
-						resultstring = term.Split('-', 2);
-						subs[l] = resultstring[l];
-						term = resultstring[l+1];
-						l++;
-					}
-				}
-				foreach(string addend in adds)
-				{
-					result += int.Parse(addend);
-				}
-				foreach(string minuend in subs)
-				{
-					result -= int.Parse(minuend);
-				}
-				return result.ToString();
-			}
+			OutputScreen_green.Text = term;
+		}
+		private string TermEvaluation(string screentext)
+		{
+			string term = screentext.Contains('(') ? BracketsResolve(screentext) : TermResolve(screentext);
+			return term;
 		}
 		private void btn_add(object sender, RoutedEventArgs e)
 		{
 			int[] numbers = new int[100];
 			int[] operation_index = new int[100];
-			string input = OutputScreen_green.Text.ToString();
-			OutputScreen_green.Text = Terms(BracketsResolve(input));
+			//string input = OutputScreen_green.Text.ToString();
+			//TermEvaluation(input);
 
 
 			//try
