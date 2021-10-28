ad9fd3075d65ed006d5fa0c28f6664ce09c342a9
Dean <dean.marinov@gmx.at>
2021-10-28 12:13:19 +0200

    v2.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,20 +11,15 @@ namespace primetest_gmp
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		string[] memory;
+		string[] memory = new string[25];
 		string[] resultstring = new string[2];
-		string[] term_split;
-		float[] term_result;
+		string[] term_split = new string[25];
+		float[] term_result = new float[4];
 		char[] separator = { '+', '-', '*', '/', '(', ')', ' ' };
 		int result_index = 0;
-		int memory_index = 0;
 		public MainWindow()
 		{
 			InitializeComponent();
-			//string input = OutputScreen_green.Text.ToString();
-			//memory = new string[input.Length];
-			//term_split = new string[input.Length];
-			//term_result = new float[4];
 		}
 		private void btn_write_click(object sender, RoutedEventArgs e)
 		{
@@ -39,7 +34,7 @@ namespace primetest_gmp
 
 		private void key_write(object sender, KeyEventArgs e)
 		{
-			switch(e.Key)
+			switch (e.Key)
 			{
 				case Key.NumPad0: { OutputScreen_green.Text += "0"; break; }
 				case Key.NumPad1: { OutputScreen_green.Text += "1"; break; }
@@ -65,6 +60,16 @@ namespace primetest_gmp
 				case Key.Add: { OutputScreen_green.Text += "+"; break; }
 				case Key.Subtract: { OutputScreen_green.Text += "-"; break; }
 				case Key.Multiply: { OutputScreen_green.Text += "*"; break; }
+				case Key.Enter: { TermEvaluation(OutputScreen_green.Text); break; }
+				case Key.Back: 
+					{  
+						if(OutputScreen_green.Text == string.Empty)
+                        {
+							break;
+                        }
+						OutputScreen_green.Text = OutputScreen_green.Text.Remove(OutputScreen_green.Text.Length - 1);
+						break; 
+					}
 			}
 		}
 
@@ -74,15 +79,15 @@ namespace primetest_gmp
 			{
 				resultstring = input.Split('(', 2);
 				term_split[0] = resultstring[0];
-				return BracketsResolve(resultstring[1]);
+				return resultstring[1];
 			}
 			else if(input.Contains(')'))
 			{
 				resultstring = input.Split(')', 2);
 				term_split[1] = resultstring[1];
-				return BracketsResolve(resultstring[0]);
+				return resultstring[0];
 			}
-			return TermResolve(resultstring[0]);
+			return input;
 		}
 		private string TermAdd(string input)
 		{
@@ -109,38 +114,21 @@ namespace primetest_gmp
 						}
 					}
 				}
-				//memory[memory_index++] = resultstring[0];
 				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
 					term_result[result_index] = int.Parse(resultstring[0]) + int.Parse(resultstring[1]);
 					input = input.Replace(string.Concat(resultstring[0] + "+" + resultstring[1]), term_result[result_index].ToString());
-					return TermAdd(input);
-				}
-				return TermEvaluation(input);
-			}
-			for(int i = 0; i < memory.Length - 1; i++)
-			{
-				if(memory[i] != null)
-				{
-					term_result[result_index] += int.Parse(memory[i]);
-					memory[i] = null;
-				}
-				else
-				{
-					result_index++;
-					break;
+                    return input;
 				}
 			}
-			return TermEvaluation(resultstring[1]);
+			return input;
 		}
 		private string TermSub(string input)
 		{
 			if(input.Contains('-'))
 			{
 				resultstring = input.Split('-', 2);
-				if(resultstring[0].All(char.IsWhiteSpace))
-					return input;
-				while(!resultstring[0].All(char.IsDigit))
+				while(!resultstring[0].All(char.IsDigit) | !resultstring[1].All(char.IsDigit))
 				{
 					foreach(char element in separator)
 					{
@@ -148,40 +136,33 @@ namespace primetest_gmp
 						{
 							memory = resultstring[0].Split(element, 2);
 							resultstring[0] = memory[1];
+							term_split[0] = memory[0];
+							break;
+						}
+						else if(resultstring[1].Contains(element))
+						{
+							memory = resultstring[1].Split(element, 2);
+							resultstring[1] = memory[0];
+							term_split[1] = memory[1];
 							break;
 						}
 					}
 				}
-				memory[memory_index++] = resultstring[0];
 				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
 					term_result[result_index] = int.Parse(resultstring[0]) - int.Parse(resultstring[1]);
-					return TermEvaluation(term_result[result_index++].ToString());
-				}
-				return TermSub(resultstring[1]);
-			}
-			for(int i = 0; i < memory.Length - 1; i++)
-			{
-				if(memory[i] != null)
-				{
-					term_result[result_index] -= int.Parse(memory[i]);
-					memory[i] = null;
-				}
-				else
-				{
-					result_index++;
-					break;
+					input = input.Replace(string.Concat(resultstring[0] + "-" + resultstring[1]), term_result[result_index].ToString());
+					return input;
 				}
 			}
-			return TermEvaluation(resultstring[1]);
+			return input;
 		}
 		private string TermMul(string input)
 		{
 			if(input.Contains('*'))
 			{
 				resultstring = input.Split('*', 2);
-				memory[memory_index++] = resultstring[0];
-				while(!resultstring[0].All(char.IsDigit))
+				while(!resultstring[0].All(char.IsDigit) | !resultstring[1].All(char.IsDigit))
 				{
 					foreach(char element in separator)
 					{
@@ -189,6 +170,14 @@ namespace primetest_gmp
 						{
 							memory = resultstring[0].Split(element, 2);
 							resultstring[0] = memory[1];
+							term_split[0] = memory[0];
+							break;
+						}
+						else if(resultstring[1].Contains(element))
+						{
+							memory = resultstring[1].Split(element, 2);
+							resultstring[1] = memory[0];
+							term_split[1] = memory[1];
 							break;
 						}
 					}
@@ -196,31 +185,18 @@ namespace primetest_gmp
 				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
 					term_result[result_index] = int.Parse(resultstring[0]) * int.Parse(resultstring[1]);
-					return TermResolve(term_result[result_index++].ToString());
-				}
-				return TermMul(resultstring[1]);
-			}
-			for(int i = 0; i < memory.Length - 1; i++)
-			{
-				if(memory[i] != null)
-				{
-					term_result[result_index] *= int.Parse(memory[i]);
-					memory[i] = null;
-				}
-				else
-				{
-					result_index++;
-					break;
+					input = input.Replace(string.Concat(resultstring[0] + "*" + resultstring[1]), term_result[result_index].ToString());
+					return input;
 				}
 			}
-			return TermEvaluation(resultstring[1]);
+			return input;
 		}
 		private string TermDiv(string input)
 		{
-			if(input.Contains('/') && resultstring[0].All(char.IsDigit))
+			if(input.Contains('/'))
 			{
 				resultstring = input.Split('/', 2);
-				while(!resultstring[0].All(char.IsDigit))
+				while(!resultstring[0].All(char.IsDigit) | !resultstring[1].All(char.IsDigit))
 				{
 					foreach(char element in separator)
 					{
@@ -228,140 +204,75 @@ namespace primetest_gmp
 						{
 							memory = resultstring[0].Split(element, 2);
 							resultstring[0] = memory[1];
+							term_split[0] = memory[0];
+							break;
+						}
+						else if(resultstring[1].Contains(element))
+						{
+							memory = resultstring[1].Split(element, 2);
+							resultstring[1] = memory[0];
+							term_split[1] = memory[1];
 							break;
 						}
 					}
 				}
-				memory[memory_index++] = resultstring[0];
 				if(resultstring[0].All(char.IsDigit) && resultstring[1].All(char.IsDigit))
 				{
 					term_result[result_index] = int.Parse(resultstring[0]) / int.Parse(resultstring[1]);
-					return term_result[result_index++].ToString();
-				}
-				return TermDiv(resultstring[1]);
-			}
-			for(int i = 0; i < memory.Length - 1; i++)
-			{
-				if(memory[i] != null)
-				{
-					term_result[result_index] /= int.Parse(memory[i]);
-					memory[i] = null;
-				}
-				else
-				{
-					result_index++;
-					break;
+					input = input.Replace(string.Concat(resultstring[0] + "/" + resultstring[1]), term_result[result_index].ToString());
+					return input;
 				}
 			}
-			return TermResolve(resultstring[1]);
+			return input;
 		}
 		private string TermResolve(string input) //returns the result of a blank term
 		{
-			if(input.Contains('*'))
-			{
-				TermMul(input);
+			if (input.Contains('('))
+            {
+				input = BracketsResolve(input);
 			}
-			memory_index = 0;
+			if(input.Contains(')'))
+            {
+				input = BracketsResolve(input);
+			}
+			if (input.Contains('*'))
+			{
+				input = TermMul(input);
+			}
 			if(input.Contains('/'))
 			{
-				TermDiv(input);
+				input = TermDiv(input);
 			}
-			memory_index = 0;
 			if(input.Contains('+'))
 			{
-				TermAdd(input);
+				input = TermAdd(input);
 			}
-			memory_index = 0;
 			if(input.Contains('-'))
 			{
-				TermSub(input);
+				input = TermSub(input);
 			}
-			memory_index = 0;
-			for(int i = 1; i < term_result.Length - 1; i++)
-			{
-				term_result[0] += term_result[i];
-				term_result[i] = 0;
-			}
-			return TermEvaluation(Convert.ToString(term_result[0]));
+			return input;
 		}
-
-		private string TermOutput(string input)
+		private void TermOutput(string input)
 		{
+			OutputScreen_green.Inlines.Clear();
 			OutputScreen_green.Text = input;
-			return OutputScreen_green.Text.ToString();
 		}
-		private string TermEvaluation(string screentext)
-		{
-			if(screentext.Contains('(') || screentext.Contains(')'))
-				BracketsResolve(screentext);
+		private void TermEvaluation(string screentext)
+		{		
 			if(!screentext.All(char.IsDigit))
-				TermResolve(screentext);
-			else
-			{
-				for(int i = 0; i < term_split.Length - 1; i++)
-				{
-					if(term_split[i] != null && (term_split[i].Contains('*') || term_split[i].Contains('/')))
-					{
-						if(i >= 1)
-						{
-							screentext = string.Concat(screentext, Convert.ToString(term_split[i]));
-							term_split[i] = null;
-						}
-						else
-						{
-							screentext = string.Concat(Convert.ToString(term_split[i]), screentext);
-							term_split[i] = null;
-						}
-						TermEvaluation(screentext);
-					}
-					if(term_split[i] == null) break;
-				}
-				for(int j = 0; j < term_result.Length - 1; j++)
-				{
-					if(term_split[j] != null && (term_split[j].Contains('+') || term_split[j].Contains('-')))
-					{
-						if(j >= 1)
-						{
-							screentext = string.Concat(screentext, Convert.ToString(term_split[j]));
-							term_split[j] = null;
-						}
-						else
-						{
-							screentext = string.Concat(Convert.ToString(term_split[j]), screentext);
-							term_split[j] = null;
-						}
-						TermEvaluation(screentext);
-					}
-				}
-			}
-			TermEvaluation(screentext);
-			return screentext = screentext.All(char.IsDigit) ? TermOutput(screentext) : TermResolve(screentext);
+				screentext = TermResolve(screentext);
+			TermOutput(screentext);
 		}
-		private void btn_add(object sender, RoutedEventArgs e)
+		private void btn_math(object sender, RoutedEventArgs e)
 		{
-			string input = OutputScreen_green.Text.ToString();
-			memory = new string[input.Length];
-			term_split = new string[input.Length];
-			term_result = new float[4];
-			TermEvaluation(input);
-			//string input = OutputScreen_green.Text.ToString();
-			//TermEvaluation(input);
+			TermEvaluation(OutputScreen_green.Text);
+		}
 
-
-			//try
-			//{
-			//	numbers = Array.ConvertAll(splitted, int.Parse);
-			//	if(numbers != null)
-			//	{
-			//		OutputScreen_green.Inlines.Clear();
-			//	}
-			//}
-			//catch
-			//{
-			//	OutputScreen_green.Inlines.Clear();
-			//	OutputScreen_green.Text = "Wrong syntax!";
-			//}
-
+		private void btn_math(object sender, KeyEventArgs e)
+		{
+			if(e.Key == Key.Enter)
+				TermEvaluation(OutputScreen_green.Text);
 		}
 	}
 }
